568289b76863e052d1ad94e097626787911792f1
Andreas Røed <andreas@Andreass-MacBook-Pro.local>
2017-11-16 20:41:40 +0100

    Initial Commit

---
+++ Favorz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Favorz
+//
+//  Created by Andreas Røed on 16/11/2017.
+//  Copyright © 2017 Andreas Røed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
